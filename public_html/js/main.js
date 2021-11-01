d3313a902e2882f0ccfcec6e3c269480081bab60
Dusan Bojan Tintor <dusannbojann@gmail.com>
2021-11-01 20:16:18 +0100

    Contact page done

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,9 +1,9 @@
 
 
 
-$(document).ready(function(){
-    
-     //ANIMATION
+$(document).ready(function () {
+
+    //ANIMATION
     function animation() {
         let windowHeight = $(window).height();
         let scroll = $(window).scrollTop();
@@ -23,25 +23,74 @@ $(document).ready(function(){
         animation();
     });
     animation();
-          // team-slider
-          
-          if($('.team-members-slider').length > 0){
-              $('.team-members-slider').owlCarousel({
-                  
-                  loop:true,
-                  autoplay:true,
-                  
-                  responsive:{
-                      0:{
-                          items:1,
-                          margin:0
-                      },
-                      992:{
-                          items:2,
-                          margin:30
-                      }
-                  }
-                  
-              });
-          }
+    // team-slider
+
+    if ($('.team-members-slider').length > 0) {
+        $('.team-members-slider').owlCarousel({
+
+            loop: true,
+            autoplay: true,
+
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 0
+                },
+                992: {
+                    items: 2,
+                    margin: 30
+                }
+            }
+
+        });
+    }
+
+    //form validator
+
+    if ($('.contact-form').length > 0) {
+
+        $('.contact-form').validate({
+
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true
+                }
+            },
+            messages: {
+                name: {
+                    required: 'The Name* field is required'
+                },
+                email: {
+                    required: 'The Email* field is required',
+                    email: 'Please provide a valid email address'
+                },
+                message: {
+                    required: 'The Message* field is required'
+                }
+            },
+
+            errorElement: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest(".form-group").find(".error-msg"));
+            }
+
+
+
+
+        });
+
+    }
 });
